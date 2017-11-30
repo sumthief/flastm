6a15fa939b010ac07fe2826397feebda8167876d
Gregory Shlyapkin <shlyapkin.g@gmail.com>
2017-11-30 23:45:12 +0700

    Simplify makeRequest method.

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,51 +1,11 @@
 const config = require('../../config');
 const axios = require('axios');
 
-const allowedMethods = ['get', 'post'];
-
-const allowedPackages = [
-  'album',
-  'artist',
-  'chart',
-  'geo',
-  'library',
-  'tag',
-  'track',
-  'user'
-];
-
 export const makeRequest = opts => {
-  const { method, _package, action, ...extraOpts } = opts;
-  const isMethodValid =
-    method &&
-    typeof method === 'string' &&
-    allowedMethods.includes(method.toLowerCase());
-  if (!isMethodValid) {
-    const processedMethods = allowedMethods
-      .map(method => `"${method.toUpperCase()}"`)
-      .join(', ');
-    throw new Error(
-      `Passed incorrect method. Please use one of these: ${processedMethods}.`
-    );
-  }
-  const isPackageValid =
-    _package &&
-    typeof _package === 'string' &&
-    allowedPackages.includes(_package.toLowerCase());
-  if (!isPackageValid) {
-    const processedPackages = allowedPackages
-      .map(_package => `"${_package}"`)
-      .join(', ');
-    throw new Error(
-      `Passed incorrect package. Please use one of these: ${processedPackages}.`
-    );
-  }
-  if (!action) {
-    throw new Error('Passed incorrect action.');
-  }
+  const { method = 'get', pkg, action, ...extraOpts } = opts;
   let data = {
     format: 'json',
-    method: `${_package}.${action}`,
+    method: `${pkg}.${action}`,
     api_key: config.api_key
   };
   Object.keys(extraOpts).forEach(key => (data[key] = extraOpts[key]));
